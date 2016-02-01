c9b6146758027ca2cb6691a0080c84203ab6435a
David Simons <12e.david@gmail.com>
2016-02-01 00:16:36 +0000

    Graphaware copyright licence notice

--- src/test/java/com/graphaware/neo4j/integration/IndexMaintenanceTest.java
+++ src/test/java/com/graphaware/neo4j/integration/IndexMaintenanceTest.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2013-2016 GraphAware
+ *
+ * This file is part of the GraphAware Framework.
+ *
+ * GraphAware Framework is free software: you can redistribute it and/or modify it under the terms of
+ * the GNU General Public License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details. You should have received a copy of
+ * the GNU General Public License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+
 package com.graphaware.neo4j.integration;
 
 import com.graphaware.neo4j.integration.helpers.ExpirationIntegrationTest;
@@ -6,9 +22,6 @@ import org.neo4j.graphdb.Transaction;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
 
 public class IndexMaintenanceTest extends ExpirationIntegrationTest {
 
@@ -45,7 +58,7 @@ public class IndexMaintenanceTest extends ExpirationIntegrationTest {
         getDatabase().execute("CREATE (p:Person {name: 'Dave', _expire: 123})");
         getDatabase().execute("MATCH (p:Person {name: 'Dave', _expire: 123}) SET p._expire = NULL RETURN p");
 
-        try(Transaction tx = getDatabase().beginTx()) {
+        try (Transaction tx = getDatabase().beginTx()) {
             assertThat(countNodesExpiringBefore1000(), equalTo(0L));
         }
     }
@@ -57,7 +70,7 @@ public class IndexMaintenanceTest extends ExpirationIntegrationTest {
 
         getDatabase().execute("MATCH (p:Person {name: 'Dave', _expire: 123}) SET p._expire = 100000 RETURN p");
 
-        try(Transaction tx = getDatabase().beginTx()) {
+        try (Transaction tx = getDatabase().beginTx()) {
             assertThat(countNodesExpiringBefore1000(), equalTo(1L));
         }
     }
--- src/test/java/com/graphaware/neo4j/integration/IndexReaderTest.java
+++ src/test/java/com/graphaware/neo4j/integration/IndexReaderTest.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2013-2016 GraphAware
+ *
+ * This file is part of the GraphAware Framework.
+ *
+ * GraphAware Framework is free software: you can redistribute it and/or modify it under the terms of
+ * the GNU General Public License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details. You should have received a copy of
+ * the GNU General Public License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+
 package com.graphaware.neo4j.integration;
 
 import com.graphaware.neo4j.config.ExpirationConfiguration;
@@ -6,7 +22,6 @@ import com.graphaware.neo4j.integration.helpers.ExpirationIntegrationTest;
 import org.junit.Test;
 
 import static org.hamcrest.Matchers.equalTo;
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 
 public class IndexReaderTest extends ExpirationIntegrationTest {
@@ -30,7 +45,6 @@ public class IndexReaderTest extends ExpirationIntegrationTest {
 
         assertThat(indexer.nodesExpiringBefore(50L).size(), equalTo(0));
     }
-
 
 
 }
